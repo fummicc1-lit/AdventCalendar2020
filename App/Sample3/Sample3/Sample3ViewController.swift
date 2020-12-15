dedecaab0557bb24bf9676b354ec59e87ae78034
fummicc1 <fumiya.tennis1@gmail.com>
2020-12-15 19:09:28 +0900

    Remove submodules

---
+++ App/Sample3/Sample3/Sample3ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Sample3ViewController.swift
+//  Sample3
+//
+//  Created by Fumiya Tanaka on 2020/12/09.
+//
+
+import UIKit
+
+class Sample3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
